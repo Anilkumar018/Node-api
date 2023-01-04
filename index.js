bb309f62222e00220f759313a4eadfd8d189e5f0
Anilkumar018 <kumbaranilkumar498@gmail.com>
2023-01-04 13:26:51 +0530

    Anil

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const arr = ['anil','roy']
-//console.log(arr);
-for(var user of arr){
-    console.log(arr);
-    console.log(user);
-}
+// const arr = ['anil','roy']
+// //console.log(arr);
+// for(var user of arr){
+//     console.log(arr);
+//     console.log(user);
+// }
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+
