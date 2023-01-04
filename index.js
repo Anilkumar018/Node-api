5a68fc6da56f44be930b6c0313a494de877cdb9e
Anilkumar018 <kumbaranilkumar498@gmail.com>
2023-01-04 14:19:57 +0530

    Anil

--- index.js
+++ index.js
@@ -11,4 +11,18 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.send("good morning teacher");
+})
+
+app.get('/student', (req,res) => {
+    res.send("ohhh, hey children");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server started on localhost : ${PORT}`));
+
 
